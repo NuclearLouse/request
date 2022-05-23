a5991d9eb857ff7b676fbd3cd7a0a16366323318
asorokin <a40in@inbox.ru>
2022-05-23 19:08:04 +0300

    Initial commit

---
+++ request.go
@@ -0,0 +1,110 @@
+package request
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Address struct {
+	Proto     string
+	Host      string
+	Path      string
+	UserPass  *url.Userinfo
+	ValuesURL url.Values
+}
+
+// http://www.example.com/api/v1/getUser?fname=foo&sname=bar
+// Proto = http, Host = www.example.com, Path = api/v1/getUser, Values: fname=foo sname=bar
+// "postgres://user:password@127.0.0.1/dbname?sslmode=disable"
+// Proto = postgres, User = user, Pass = password, Host = 127.0.0.1, Path = dbname, Values = sslmode=disable
+func NewAddress(proto, host string, userPass ...string) *Address {
+	var userinfo *url.Userinfo
+	switch len(userPass) {
+	case 1:
+		userinfo = url.UserPassword(userPass[0], "")
+	case 2:
+		userinfo = url.UserPassword(userPass[0], userPass[1])
+	}
+	return &Address{
+		Proto:    proto,
+		Host:     host,
+		UserPass: userinfo,
+	}
+}
+
+func (a *Address) SetEndpoint(path string, keyval ...interface{}) *Address {
+	a.Path = path
+	if len(keyval) >= 2 {
+		args := make(url.Values, len(keyval)/2+1)
+		switch len(keyval) {
+		case 2:
+			args.Set(fmt.Sprintf("%v", keyval[0]), fmt.Sprintf("%v", keyval[1]))
+		default:
+			for i := 0; i < len(keyval); i += 2 {
+				if (i + 1) == len(keyval) {
+					break
+				}
+				args.Set(fmt.Sprintf("%v", keyval[i]), fmt.Sprintf("%v", keyval[i+1]))
+			}
+		}
+		a.ValuesURL = args
+	}
+	return a
+}
+
+func (a *Address) URL() *url.URL {
+	return &url.URL{
+		Scheme:   a.Proto,
+		Host:     a.Host,
+		Path:     a.Path,
+		User:     a.UserPass,
+		RawQuery: a.ValuesURL.Encode(),
+	}
+}
+
+func (a *Address) String() string {
+	return a.URL().String()
+}
+
+type Params struct {
+	Method string
+	URL    *url.URL
+	Body   io.Reader
+	Header map[string]string
+	Client *http.Client
+}
+
+//Default: Method = GET, Client.Timeout = 5s
+func DoRequestDefault(url string, body []byte) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, url, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+	cl := &http.Client{
+		Timeout: time.Duration(5) * time.Second,
+	}
+	return cl.Do(req)
+}
+
+func (p *Params) DoRequestWithParams() (*http.Response, error) {
+	if p.Client == nil {
+		p.Client = http.DefaultClient
+	}
+
+	//bytes.NewBuffer(p.Body)
+	req, err := http.NewRequest(p.Method, p.URL.String(), p.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if p.Header != nil {
+		for key, val := range p.Header {
+			req.Header.Set(key, val)
+		}
+	}
+	return p.Client.Do(req)
+}
---
+++ request_test.go
@@ -0,0 +1,193 @@
+package request
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakeEndpointAddress(t *testing.T) {
+	endpoint := "dbname"
+
+	testCases := []struct {
+		name     string
+		address  *Address
+		keyval   []interface{}
+		expected string
+	}{
+		{
+			name:     "Эндпоинт без параметров запроса",
+			address:  NewAddress("postgres", "localhost"),
+			keyval:   nil,
+			expected: "postgres://localhost/dbname",
+		},
+		{
+			name:     "Эндпоинт с параметрами запроса",
+			address:  NewAddress("postgres", "localhost"),
+			keyval:   []interface{}{"sslmode", "disable"},
+			expected: "postgres://localhost/dbname?sslmode=disable",
+		},
+		{
+			name:     "Эндпоинт с юзер-инфо и параметрами",
+			address:  NewAddress("postgres", "localhost", "user", "password"),
+			keyval:   []interface{}{"sslmode", "disable"},
+			expected: "postgres://user:password@localhost/dbname?sslmode=disable",
+		},
+		{
+			name:     "Эндпоинт с юзер-инфо и нечетным числом параметров",
+			address:  NewAddress("postgres", "localhost", "user", "password"),
+			keyval:   []interface{}{"sslmode", "disable", "pool_max_conns"},
+			expected: "postgres://user:password@localhost/dbname?sslmode=disable",
+		},
+		{
+			name:     "Зндпоинт с разнотипными параметрами",
+			address:  NewAddress("postgres", "localhost", "user", "password"),
+			keyval:   []interface{}{"sslmode", "disable", "pool_max_conns", 25, "bool_key", true},
+			expected: "postgres://user:password@localhost/dbname?bool_key=true&pool_max_conns=25&sslmode=disable",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, tc.address.SetEndpoint(endpoint, tc.keyval...).String())
+		})
+	}
+}
+
+func TestRequest(t *testing.T) {
+	addr := NewAddress("http", "pie.dev")
+
+	res, err := DoRequestDefault(addr.SetEndpoint("get").String(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusOK, res.StatusCode, "Дефолтный запрос")
+
+	type bodyRequest struct {
+		Key string `json:"key"`
+	}
+
+	body := bodyRequest{
+		Key: "value",
+	}
+
+	b := new(bytes.Buffer)
+	if err := json.NewEncoder(b).Encode(body); err != nil {
+		t.Fatal(err)
+	}
+
+	testCases := []struct {
+		name           string
+		params         *Params
+		expectedCode   int
+		expectedBody   string
+		expectedHeader string
+	}{
+		{
+			name: "Не дефолтный метод с телом запроса",
+			params: &Params{
+				Method: http.MethodPost,
+				URL:    addr.SetEndpoint("post").URL(),
+				Body:   b,
+			},
+			expectedCode:   http.StatusOK,
+			expectedBody:   "value",
+			expectedHeader: "Go-http-client/1.1",
+		},
+		{
+			name: "С установкой хедера",
+			params: &Params{
+				Method: http.MethodGet,
+				URL:    addr.SetEndpoint("headers").URL(),
+				Header: map[string]string{
+					"User-Agent": "Bacon/1.0",
+				},
+				Body: nil,
+			},
+			expectedCode:   http.StatusOK,
+			expectedHeader: "Bacon/1.0",
+		},
+	}
+
+	type bodyResponse struct {
+		Headers struct {
+			Cookie    string `json:"Cookie"`
+			UserAgent string `json:"User-Agent"`
+			XFoo      string `json:"X-Foo"`
+		} `json:"headers"`
+		Json struct {
+			Key string `json:"key"`
+		} `json:"json"`
+		Origin string   `json:"origin"`
+		Url    string   `json:"url"`
+		Args   struct{} `json:"args,omitempty"`
+		Data   string   `json:"data,omitempty"`
+		Files  struct{} `json:"files,omitempty"`
+		Form   struct{} `json:"form,omitempty"`
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := tc.params.DoRequestWithParams()
+			if err != nil {
+				t.Fatal(err)
+			}
+			assert.Equal(t, tc.expectedCode, res.StatusCode)
+
+			var body bodyResponse
+			if err := json.NewDecoder(res.Body).Decode(&body); err != nil {
+				t.Fatal(err)
+			}
+			defer res.Body.Close()
+			assert.Equal(t, tc.expectedHeader, body.Headers.UserAgent)
+			assert.Equal(t, tc.expectedBody, body.Json.Key)
+		})
+	}
+}
+
+/*
+	prms := &Params{
+		Method: http.MethodPost,
+		URL:    addr.SetEndpoint("post").URL(),
+	}
+
+	res, err = prms.DoRequestWithParams()
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	prms = &Params{
+		Method: http.MethodGet,
+		URL:    addr.SetEndpoint("headers").URL(),
+		Header: map[string]string{
+			"Cookie":     "valued-visitor=yes;foo=bar",
+			"User-Agent": "Bacon/1.0",
+			"X-Foo":      "Bar",
+		},
+	}
+
+	res, err = prms.DoRequestWithParams()
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	var bodyJSON struct {
+		Headers struct {
+			Cookie    string `json:"Cookie"`
+			UserAgent string `json:"User-Agent"`
+			XFoo      string `json:"X-Foo"`
+		} `json:"headers"`
+		JSON struct {
+			Key string `json:"key"`
+		} `json:"json"`
+	}
+	if err := json.NewDecoder(res.Body).Decode(&bodyJSON); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "valued-visitor=yes;foo=bar", bodyJSON.Headers.Cookie)
+*/
